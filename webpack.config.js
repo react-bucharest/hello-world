c9e74a214db0b01eec4118700dd736398a9ea2a3
Gabi Purcaru <gabi@purcaru.com>
2016-06-14 12:12:13 +0300

    added react babel preset

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel', query: { presets: ['es2015'] } }
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel', query: { presets: ['es2015', 'react'] } }
     ]
   },
   plugins: [
