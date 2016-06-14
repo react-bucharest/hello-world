7e38873cc8c854ee346332cf2886cdf4f0c2acac
Dan Jipa <dan.jipa@gmail.com>
2016-06-14 12:46:25 +0300

    adding API key for google translate service; adding drop down for target language, fetch method

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import 'whatwg-fetch';
+import { API_KEY } from '../config';
 
-const App = () => (
-  <div>
-  	<input placeholder="hello world"/>
-  </div>
-)
+let App = React.createClass({
+  render: function() {
+    return <div>
+		  	<input type="text" id="message" placeholder="hello world"/>
+		  	<select id="target">
+		  		<option value="ro">Romana</option>
+		  		<option value="fr">Franceza</option>
+		  		<option value="ru">Rusa</option>
+			</select>
+			<button onClick={this.translate()}>Translate</button>
+			<br/>
+			<input type="text" id="translation"/>
+		   </div>
+  	},
+  	translate: function(){
+  		fetch('https://www.googleapis.com/language/translate/v2?key='	+	API_KEY	+	'	&source=en&target=de&q=Hello%20world')
+      		.then(response => response.json())
+  	}
+});
 
 export default App
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const message = 'Hello World!';
+export const API_KEY = 'AIzaSyAtaox5l1corFKcEywiOit4xKGW1mLsjGA';
