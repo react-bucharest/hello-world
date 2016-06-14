6196baa3bbaebfe37833bd3c5016a5c3713b5540
Dan Jipa <dan.jipa@gmail.com>
2016-06-14 17:17:38 +0300

    adding middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers'
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
   return store
 }
